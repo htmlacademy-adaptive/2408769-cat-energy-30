0009ab3a6d4de87b5b6dfa7287da0516b532fa98
IgorRichter <i.rixter201572@gmail.com>
2023-12-30 22:15:23 +0300

    Исправляет ошибки линтера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,33 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 
-  var nav = document.querySelector('.main-nav');
-  var toggleButton = document.querySelector('.button-toogle');
+//   const nav = document.querySelector('.main-nav');
+//   const toggleButton = document.querySelector('.button-toogle');
 
-  nav.classList.add('main-nav--closed');
-  toggleButton.classList.add('button-toogle--visible');
+//   nav.classList.add('main-nav--closed');
+//   toggleButton.classList.add('button-toogle--visible');
 
-  toggleButton.addEventListener('click', function () {
-    nav.classList.toggle('main-nav--closed');
-    nav.classList.toggle('main-nav--open');
+//   toggleButton.addEventListener('click', function () {
+//     nav.classList.toggle('main-nav--closed');
+//     nav.classList.toggle('main-nav--open');
 
-    toggleButton.classList.toggle('button-toogle--visible');
-    toggleButton.classList.toggle('button-toogle--close');
+//     toggleButton.classList.toggle('button-toogle--visible');
+//     toggleButton.classList.toggle('button-toogle--close');
+//   });
+
+//   document.addEventListener('click', function (event) {
+//     const isClickInsideNav = nav.contains(event.target);
+//     const isClickOnToggleButton = toggleButton.contains(event.target);
+
+//     if (!isClickInsideNav && !isClickOnToggleButton) {
+
+//       nav.classList.remove('main-nav--open');
+//       nav.classList.add('main-nav--closed');
+
+//       toggleButton.classList.remove('button-toogle--close');
+//       toggleButton.classList.add('button-toogle--visible');
+//     }
+//   });
+// });
+
+
+document.addEventListener('DOMContentLoaded',
+  () => {
+    const nav = document.querySelector('.main-nav');
+    const toggleButton = document.querySelector('.button-toogle');
+
+    nav.classList.add('main-nav--closed');
+    toggleButton.classList.add('button-toogle--visible');
+
+    toggleButton.addEventListener('click',
+      () => {
+        nav.classList.toggle('main-nav--closed');
+        nav.classList.toggle('main-nav--open');
+
+        toggleButton.classList.toggle('button-toogle--visible');
+        toggleButton.classList.toggle('button-toogle--close');
+      });
+
+    document.addEventListener('click',
+      (event) => {
+        const isClickInsideNav = nav.contains(event.target);
+        const isClickOnToggleButton = toggleButton.contains(event.target);
+
+        if (!isClickInsideNav && !isClickOnToggleButton) {
+          nav.classList.remove('main-nav--open');
+          nav.classList.add('main-nav--closed');
+
+          toggleButton.classList.remove('button-toogle--close');
+          toggleButton.classList.add('button-toogle--visible');
+        }
+      });
   });
-
-  document.addEventListener('click', function (event) {
-    var isClickInsideNav = nav.contains(event.target);
-    var isClickOnToggleButton = toggleButton.contains(event.target);
-
-    if (!isClickInsideNav && !isClickOnToggleButton) {
-
-      nav.classList.remove('main-nav--open');
-      nav.classList.add('main-nav--closed');
-
-      toggleButton.classList.remove('button-toogle--close');
-      toggleButton.classList.add('button-toogle--visible');
-    }
-  });
-});
-
-
-

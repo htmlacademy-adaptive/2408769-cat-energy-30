a603d1ff6b0d7bd0fe93ef5556789a9797d109c2
IgorRichter <i.rixter201572@gmail.com>
2023-12-30 21:32:19 +0300

    Добавляет реализацию js для меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,33 @@
-/* в этот файл добавляет скрипты*/
+document.addEventListener('DOMContentLoaded', function () {
+
+  var nav = document.querySelector('.main-nav');
+  var toggleButton = document.querySelector('.button-toogle');
+
+  nav.classList.add('main-nav--closed');
+  toggleButton.classList.add('button-toogle--visible');
+
+  toggleButton.addEventListener('click', function () {
+    nav.classList.toggle('main-nav--closed');
+    nav.classList.toggle('main-nav--open');
+
+    toggleButton.classList.toggle('button-toogle--visible');
+    toggleButton.classList.toggle('button-toogle--close');
+  });
+
+  document.addEventListener('click', function (event) {
+    var isClickInsideNav = nav.contains(event.target);
+    var isClickOnToggleButton = toggleButton.contains(event.target);
+
+    if (!isClickInsideNav && !isClickOnToggleButton) {
+
+      nav.classList.remove('main-nav--open');
+      nav.classList.add('main-nav--closed');
+
+      toggleButton.classList.remove('button-toogle--close');
+      toggleButton.classList.add('button-toogle--visible');
+    }
+  });
+});
+
+
+

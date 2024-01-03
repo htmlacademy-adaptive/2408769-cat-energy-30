4e2807ef74d2a44456a8ffb39aa202155064cb5b
IgorRichter <i.rixter201572@gmail.com>
2024-01-03 20:00:52 +0300

    Правки по проекту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,35 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-
-//   const nav = document.querySelector('.main-nav');
-//   const toggleButton = document.querySelector('.button-toogle');
-
-//   nav.classList.add('main-nav--closed');
-//   toggleButton.classList.add('button-toogle--visible');
-
-//   toggleButton.addEventListener('click', function () {
-//     nav.classList.toggle('main-nav--closed');
-//     nav.classList.toggle('main-nav--open');
-
-//     toggleButton.classList.toggle('button-toogle--visible');
-//     toggleButton.classList.toggle('button-toogle--close');
-//   });
-
-//   document.addEventListener('click', function (event) {
-//     const isClickInsideNav = nav.contains(event.target);
-//     const isClickOnToggleButton = toggleButton.contains(event.target);
-
-//     if (!isClickInsideNav && !isClickOnToggleButton) {
-
-//       nav.classList.remove('main-nav--open');
-//       nav.classList.add('main-nav--closed');
-
-//       toggleButton.classList.remove('button-toogle--close');
-//       toggleButton.classList.add('button-toogle--visible');
-//     }
-//   });
-// });
-
-
 document.addEventListener('DOMContentLoaded',
   () => {
     const nav = document.querySelector('.main-nav');
